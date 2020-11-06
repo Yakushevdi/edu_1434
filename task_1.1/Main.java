2d3a060faa96d12a0fff1efae08b00b29f771692
Yakushevdi <73994430+Yakushevdi@users.noreply.github.com>
2020-11-06 15:56:12 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Денис";
+        int age = 30;
+        String city = "Москва";
+
+        System.out.println("Имя: "+name);
+        System.out.println("Город: "+city);
+        System.out.println("Возраст: "+age);
+
+    }
+}
