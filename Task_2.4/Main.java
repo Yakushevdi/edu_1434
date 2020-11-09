9e1dc16815801df5fb917a0a89cb749675e13a1e
Yakushevdi <73994430+Yakushevdi@users.noreply.github.com>
2020-11-09 17:24:15 +0300

    Create Main.java

---
+++ Task_2.4/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Сумма вклада: ");
+        float summ = scanner.nextInt();
+        System.out.print("Годовой процент по вкладу на 5 лет: ");
+        float percent = scanner.nextInt();
+
+        for (int i = 1; i < 6; i++) {
+            summ=summ*(percent/100+1);
+            System.out.println("Сумма вклада "+i+" год: "+ Math.round(summ));
+        }
+    }
+}
